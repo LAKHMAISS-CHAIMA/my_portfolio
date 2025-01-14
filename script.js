933e520637629d29f881e440bb0fed1367d510b4
LAKHMAISS-CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-01-13 18:24:32 -0800

    second version

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-$
+
+$(document).ready(function() {
+    $('header nav a').on('click', function(event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+    });
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        alert('Merci de nous avoir contactés. Nous vous répondrons bientôt !');
+        $(this).trigger('reset');
+    });
+});
+
